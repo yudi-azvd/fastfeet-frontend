269d1f1ed066d89eba2833ae2c4005d3a81192f7
Yudi Yamane <yudiyamane@gmail.com>
2020-03-09 15:30:33 -0300

    changed persist key

--- src/store/persistReducers.js
+++ src/store/persistReducers.js
@@ -4,7 +4,7 @@ import { persistReducer } from 'redux-persist';
 export default reducers => {
   const persistedReducer = persistReducer(
     {
-      key: 'gobarber',
+      key: 'fastfeet',
       storage,
       // informações que vão ser persistidas
       whitelist: ['auth', 'user'],
