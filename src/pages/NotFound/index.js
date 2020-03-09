fe9042e2e855d90bf909029a609e09ad8e2991d3
Yudi Yamane <yudiyamane@gmail.com>
2020-03-09 01:00:49 -0300

    added simple not found page

---
+++ src/pages/NotFound/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+export default function NotFound() {
+  return (
+    <div>
+      <div>
+        <h1>Página não encontrada :(</h1>
+      </div>
+    </div>
+  );
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -5,12 +5,16 @@ import Route from './Route';
 
 import SignIn from '../pages/SignIn';
 import Deliveries from '../pages/Deliveries';
+import NotFound from '../pages/NotFound';
 
 export default function Routes() {
   return (
     <Switch>
       <Route exact path="/" component={SignIn} />
       <Route path="/deliveries" component={Deliveries} isPrivate />
+
+      {/* Essa rota é privada ou não? */}
+      <Route path="*" component={NotFound} />
     </Switch>
   );
 }
