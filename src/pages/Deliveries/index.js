c157962d74ec30e4a719de4bd31449fbfa84c708
Yudi Yamane <yudiyamane@gmail.com>
2020-03-09 23:34:05 -0300

    grid display to show deliveries. Not fully done

--- src/pages/Deliveries/index.js
+++ src/pages/Deliveries/index.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
+import { FiMoreHorizontal } from 'react-icons/fi';
 
 import api from '../../services/api';
 
-import { Container } from './styles';
+import { Container, DeliveriesList } from './styles';
 
 export default function Deliveries() {
   const [deliveries, setDeliveries] = useState([]);
@@ -26,20 +27,46 @@ export default function Deliveries() {
         <button type="button">CADASTRAR</button>
       </div>
 
-      <ul>
-        {deliveries.map(d => (
-          <li key={d.id}>
-            <p> {d.id} </p>
-            <p> {d.recipient.name} </p>
-            <p> {d.deliveryman.name} </p>
-            <p> {d.recipient.city} </p>
-            <p> {d.recipient.state} </p>
-            <p> {d.status} </p>
-            <p> {d.id} </p>
-            <hr />
-          </li>
-        ))}
-      </ul>
+      <DeliveriesList>
+        <div className="row">
+          <div>ID</div>
+          <div>Destinatário</div>
+          <div>Recipiente</div>
+          <div>Cidade</div>
+          <div>Estado</div>
+          <div>Status</div>
+          <div className="actions">Ações</div>
+        </div>
+
+        <ul>
+          {deliveries.map(d => (
+            <li className="row" key={`delivery-${d.id}`}>
+              <div> #{d.id} </div>
+              <div> {d.recipient.name} </div>
+              <div className="avatar">
+                <img
+                  src="https://api.adorable.io/avatars/40/abott@adorable.png"
+                  alt="imagem de perfil"
+                />
+                <span>{d.deliveryman.name}</span>
+              </div>
+              <div> {d.recipient.city} </div>
+              <div> {d.recipient.state} </div>
+              {/* status é calculado! */}
+              <div> status</div>
+              <div className="actions">
+                {/* <button type="button"> */}
+                <FiMoreHorizontal
+                  onClick={() => console.log('actions')}
+                  size={16}
+                  color="#666"
+                />
+                {/* </button> */}
+              </div>
+            </li>
+          ))}
+        </ul>
+      </DeliveriesList>
     </Container>
   );
 }
--- src/pages/Deliveries/styles.js
+++ src/pages/Deliveries/styles.js
@@ -16,6 +16,7 @@ export const Container = styled.div`
 
     input {
       /* background: #fff url('../assets/search.svg') no-repeat 190px center; */
+      margin-bottom: 22px;
       padding-left: 16px;
       width: 237px;
       height: 36px;
@@ -28,6 +29,76 @@ export const Container = styled.div`
       /* TEMPORÁRIO ENQT NÃO HÁ COPNNT */
       background: purple;
       color: white;
+    }
+  }
+`;
+
+export const DeliveriesList = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  div.row {
+    margin-bottom: 14px;
+    font-weight: bold;
+    color: #444;
+
+    div.actions {
+      display: flex;
+      justify-content: flex-end;
+      align-items: center;
+    }
+  }
+
+  .row {
+    display: grid;
+    grid-template-columns: 0.5fr 1fr 1fr 1fr 1fr 1fr 1fr;
+    padding-left: 25px;
+  }
+
+  /* Não to sabendo alinhar Ações com o ícone de ação */
+  div.row {
+    padding-right: 28px;
+  }
+
+  li.row {
+    padding-right: 28px;
+  }
+
+  ul {
+    li {
+      /* aí quebra */
+      line-height: 57px;
+      /* vertical-align: baseline; */
+
+      color: #666;
+      background: #fff;
+      border-radius: 4px;
+      height: 57px;
+      margin-bottom: 21px;
+      overflow-wrap: normal;
+
+      div.avatar {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        img {
+          width: 35px;
+          height: 35px;
+          border-radius: 50%;
+          margin-right: 5px;
+        }
+      }
+
+      div.actions {
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+
+        svg {
+          cursor: pointer;
+        }
+      }
     }
   }
 `;
