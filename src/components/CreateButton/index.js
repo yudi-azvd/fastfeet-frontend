609ba171b9c9501d85562b2b51588c37ddd02f8b
Yudi Yamane <yudiyamane@gmail.com>
2020-03-10 16:44:58 -0300

    add link prop to button

--- src/components/CreateButton/index.js
+++ src/components/CreateButton/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import { GoPlus } from 'react-icons/go';
 
 import { Button } from './styles';
 
 // tem que receber um link
-export default function CreateButton() {
+export default function CreateButton({ link }) {
   return (
-    <Button type="button">
-      <GoPlus size={16} /> CADASTRAR
-    </Button>
+    <Link to={link}>
+      <Button type="button">
+        <GoPlus size={16} /> CADASTRAR
+      </Button>
+    </Link>
   );
 }
+
+CreateButton.propTypes = {
+  link: PropTypes.string.isRequired,
+};
