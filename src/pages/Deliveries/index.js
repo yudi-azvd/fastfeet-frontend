1000cc8940a79bb1ffdd9c6a7e20aa891f673d8c
Yudi Yamane <yudiyamane@gmail.com>
2020-03-22 03:15:39 -0300

    started implementing modal

--- src/pages/Deliveries/index.js
+++ src/pages/Deliveries/index.js
@@ -4,10 +4,9 @@ import React, { useState, useEffect } from 'react';
 import { FiMoreHorizontal } from 'react-icons/fi';
 import { IoMdEye } from 'react-icons/io';
 import { MdEdit, MdDeleteForever } from 'react-icons/md';
-
 import api from '../../services/api';
 
-import { Container, DeliveriesList, DeliveryItem } from './styles';
+import { Container, DeliveriesList, DeliveryItem, Modal } from './styles';
 
 import CreateButton from '../../components/CreateButton';
 import DefaultAvatar from '../../components/DefaultAvatar';
@@ -27,6 +26,8 @@ export default function Deliveries() {
   const [deliveries, setDeliveries] = useState([]);
   const [productQuery, setProductQuery] = useState('');
   const [openDeliveryActionsId, setOpenDeliveryActionsId] = useState(0);
+  const [openModal, setOpenModal] = useState(!true);
+  const [modalDelivery, setModalDelivery] = useState({});
 
   useEffect(() => {
     async function loadDeliveries() {
@@ -61,86 +62,123 @@ export default function Deliveries() {
     }
   }
 
-  function handleView(delivery) {}
+  function handleView(delivery) {
+    setModalDelivery(delivery);
+    setOpenModal(true);
+  }
   function handleEdit(deliveryId) {}
   function handleDelete(deliveryId) {}
 
   return (
-    <Container>
-      <h1>Gerenciando encomendas</h1>
+    <>
+      <Container>
+        <h1>Gerenciando encomendas</h1>
 
-      <div>
-        <input
-          type="text"
-          placeholder="Buscar por encomendas"
-          value={productQuery}
-          onChange={handleProductQueryChange}
-        />
-        <CreateButton link="/deliveries/new" />
-      </div>
-
-      <DeliveriesList>
-        <div className="header">
-          <div>ID</div>
-          <div>Destinatário</div>
-          <div>Entregador</div>
-          <div>Cidade</div>
-          <div>Estado</div>
-          <div>Status</div>
-          <div className="actions">Ações</div>
+        <div>
+          <input
+            type="text"
+            placeholder="Buscar por encomendas"
+            value={productQuery}
+            onChange={handleProductQueryChange}
+          />
+          <CreateButton link="/deliveries/new" />
         </div>
 
-        <ul>
-          {deliveries.map((d, index) => (
-            // https://dmitripavlutin.com/use-react-memo-wisely/
-            <DeliveryItem key={`delivery-${d.id}`}>
-              <div> #{d.id} </div>
-              <div> {d.recipient.name} </div>
-              <div className="avatar">
-                {d.deliveryman.avatar ? (
-                  <img // aí vem a URL do avatar
-                    src="https://api.adorable.io/avatars/40/abott@adorable.png"
-                    alt="imagem de perfil"
-                  />
-                ) : (
-                  <DefaultAvatar
-                    initials={d.deliveryman.name[0]}
-                    color={colors[index % colors.length]}
-                  />
-                )}
-                <span>{d.deliveryman.name}</span>
-              </div>
-              <div> {d.recipient.city} </div>
-              <div> {d.recipient.state} </div>
-              <div className="status">
-                <DeliveryStatus delivery={d} />
-              </div>
-              <div className="actions">
-                <div className="dropdown">
-                  <FiMoreHorizontal
-                    size={16}
-                    color="#666"
-                    onClick={() => toggleActionsModalVisibility(d)}
-                  />
-                  <ActionsDropdown open={d.id === openDeliveryActionsId}>
-                    <li onClick={() => handleView(d)}>
-                      <IoMdEye size={15} color="#8E5BE8" />
-                      <span>Visualizar</span>
-                    </li>
-                    <li onClick={() => handleEdit(d.id)}>
-                      <MdEdit size={16} color="#4D85EE" /> <span>Editar</span>
-                    </li>
-                    <li onClick={() => handleDelete(d.id)}>
-                      <MdDeleteForever size={16} color="#DE3B3B" />
-                      <span>Excluir</span>
-                    </li>
-                  </ActionsDropdown>
+        <DeliveriesList>
+          <div className="header">
+            <div>ID</div>
+            <div>Destinatário</div>
+            <div>Entregador</div>
+            <div>Cidade</div>
+            <div>Estado</div>
+            <div>Status</div>
+            <div className="actions">Ações</div>
+          </div>
+
+          <ul>
+            {deliveries.map((d, index) => (
+              // https://dmitripavlutin.com/use-react-memo-wisely/
+              <DeliveryItem key={`delivery-${d.id}`}>
+                <div> #{d.id} </div>
+                <div> {d.recipient.name} </div>
+                <div className="avatar">
+                  {d.deliveryman.avatar ? (
+                    <img // aí vem a URL do avatar
+                      src="https://api.adorable.io/avatars/40/abott@adorable.png"
+                      alt="imagem de perfil"
+                    />
+                  ) : (
+                    <DefaultAvatar
+                      initials={d.deliveryman.name[0]}
+                      color={colors[index % colors.length]}
+                    />
+                  )}
+                  <span>{d.deliveryman.name}</span>
                 </div>
-              </div>
-            </DeliveryItem>
-          ))}
-        </ul>
-      </DeliveriesList>
-    </Container>
+                <div> {d.recipient.city} </div>
+                <div> {d.recipient.state} </div>
+                <div className="status">
+                  <DeliveryStatus delivery={d} />
+                </div>
+                <div className="actions">
+                  <div className="dropdown">
+                    <FiMoreHorizontal
+                      size={16}
+                      color="#666"
+                      onClick={() => toggleActionsModalVisibility(d)}
+                    />
+                    <ActionsDropdown open={d.id === openDeliveryActionsId}>
+                      <li onClick={() => handleView(d)}>
+                        <IoMdEye size={15} color="#8E5BE8" />
+                        <span>Visualizar</span>
+                      </li>
+                      <li onClick={() => handleEdit(d.id)}>
+                        <MdEdit size={16} color="#4D85EE" /> <span>Editar</span>
+                      </li>
+                      <li onClick={() => handleDelete(d.id)}>
+                        <MdDeleteForever size={16} color="#DE3B3B" />
+                        <span>Excluir</span>
+                      </li>
+                    </ActionsDropdown>
+                  </div>
+                </div>
+              </DeliveryItem>
+            ))}
+          </ul>
+        </DeliveriesList>
+      </Container>
+
+      {openModal && (
+        <Modal isOpen={openModal}>
+          <div id="modal-content">
+            <section>
+              <p>
+                <strong>Informações da encomenda</strong>
+              </p>
+              <p> {modalDelivery.recipient.city}</p>
+              <p> {modalDelivery.recipient.state} </p>
+              <p> {modalDelivery.recipient.cep} </p>
+            </section>
+            <section>
+              <p>
+                <strong>Datas</strong>
+              </p>
+              <p>
+                <span>Retirada: </span> {modalDelivery.formattedStartDate}
+              </p>
+              <p>
+                <span>Entrega: </span>
+                {modalDelivery.formattedEndDate}
+              </p>
+            </section>
+            <section>
+              <p>
+                <strong>Assinatura do destinatário</strong>
+              </p>
+            </section>
+          </div>
+        </Modal>
+      )}
+    </>
   );
 }
--- src/pages/Deliveries/styles.js
+++ src/pages/Deliveries/styles.js
@@ -122,3 +122,58 @@ export const DeliveryItem = memo(styled.li`
     }
   }
 `);
+
+export const Modal = styled.div`
+  display: ${props => (props.isOpen ? 'block' : 'none')};
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: rgba(0, 0, 0, 0.7);
+  z-index: 5;
+
+  div#modal-content {
+    position: absolute;
+    /* top e bottom precisam estar deifinidos 
+    pra que uma altura esteja definida (?). 
+    left e right precisam estar definidos p que
+    ele tenha comprimento. */
+    top: 0px;
+    left: 40px;
+    right: 40px;
+    bottom: 10px;
+    box-shadow: 0px 0px 10px #00000033;
+    background: #fff;
+    overflow: auto;
+    border-radius: 4px;
+    margin: 238px auto;
+    width: 450px;
+    min-height: 253px;
+    padding: 25px;
+
+    section {
+      display: block;
+
+      & + section {
+        margin-top: 10px;
+        padding-top: 10px;
+        border-top: 1px solid #eee;
+      }
+
+      p {
+        color: #666;
+        margin-bottom: 8px;
+
+        strong {
+          color: #444;
+        }
+
+        span {
+          font-weight: bold;
+          color: #666;
+        }
+      }
+    }
+  }
+`;
