73e3d1f4ec8cc523541fa88ad0c4291e6d3166a5
Yudi Yamane <yudiyamane@gmail.com>
2020-03-23 19:03:09 -0300

    added comments about clicking outside behavior

--- src/components/BasicModal/index.js
+++ src/components/BasicModal/index.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 
 import { Container } from './styles';
 
+/* CLICK OUTSIDE */
+/* https://medium.com/@pitipatdop/little-neat-trick-to-capture-click-outside-with-react-hook-ba77c37c7e82 */
 export default function BasicModal({ open, children, className }) {
   return (
     <Container className={`${className} modal-component-container`} open={open}>
