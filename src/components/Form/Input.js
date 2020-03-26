1e18c9d773c7b0e8901c78e2c843913d6217b992
Yudi Yamane <yudiyamane@gmail.com>
2020-03-26 19:48:16 -0300

    created Input

---
+++ src/components/Form/Input.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useRef } from 'react';
+import { useField } from '@unform/core';
+
+// import { Container } from './styles';
+
+export default function Input({ name, ...rest }) {
+  const inputRef = useRef(null);
+  const { fieldName, defaultValue, registerField, error } = useField(name);
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef.current,
+      path: 'value',
+    });
+  }, [fieldName, registerField]);
+
+  return (
+    <>
+      <input ref={inputRef} type="text" {...rest} />
+    </>
+  );
+}
