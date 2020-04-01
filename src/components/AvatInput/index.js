680809f773aa740dd8d242ac8e7103d6883375b9
Yudi Yamane <yudiyamane@gmail.com>
2020-04-01 16:20:19 -0300

    created avatar input according to unform documentation

--- src/components/AvatInput/index.js
+++ src/components/AvatInput/index.js
@@ -1,18 +1,50 @@
-import React, {
-  ChangeEvent,
-  useRef,
-  useEffect,
-  useCallback,
-  useState,
-} from 'react';
+import React, { useRef, useEffect, useCallback, useState } from 'react';
+import PropTypes from 'prop-types';
 import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
-export default function AvatInput() {
+export default function AvatInput({ name, ...rest }) {
+  const inputRef = useRef(null);
+  const { fieldName, registerField, defaultValue, error } = useField(name);
+  const [preview, setPreview] = useState(defaultValue);
+
+  const handlePreview = useCallback(event => {
+    const file = event.target.files ? event.target.files[0] : null;
+
+    if (!file) {
+      setPreview(null);
+    }
+
+    const previewURL = URL.createObjectURL(file);
+
+    setPreview(previewURL);
+  }, []);
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef.current,
+      path: 'values[0]',
+      clearValue(ref) {
+        ref.value = '';
+        setPreview(null);
+      },
+      setValue(_, value) {
+        setPreview(value);
+      },
+    });
+  }, [fieldName, registerField]);
+
   return (
     <Container>
-      <input type="text" />
+      {preview && <img src={preview} alt="Preview" width="100" />}
+      <input ref={inputRef} onChange={handlePreview} type="file" {...rest} />
+      {error && <span className="error">{error}</span>}
     </Container>
   );
 }
+
+AvatInput.propTypes = {
+  name: PropTypes.string.isRequired,
+};
