753397a13c746028d021a86cd8ee65698998a048
Yudi Yamane <yudiyamane@gmail.com>
2020-04-08 14:10:36 -0300

    refactor: components decides wheter to show avatar pic ow a default avatar based on deliveryman props

--- src/components/MiniDefaultAvatar/index.js
+++ src/components/MiniDefaultAvatar/index.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Span } from './styles';
+import { Container } from './styles';
 
-export default function MiniDefaultAvatar({ initials, color }) {
-  return <Span color={color}> {initials} </Span>;
+export default function MiniDefaultAvatar({ deliveryman, color }) {
+  return (
+    <Container color={color}>
+      {deliveryman.avatar_id ? (
+        <img src={deliveryman.avatar.url} alt="imagem de perfil" />
+      ) : (
+        <span> {deliveryman.name.slice(0, 2)} </span>
+      )}
+    </Container>
+  );
 }
 
 MiniDefaultAvatar.propTypes = {
-  initials: PropTypes.string.isRequired,
   color: PropTypes.string.isRequired,
+  deliveryman: PropTypes.shape({
+    name: PropTypes.string,
+    avatar_id: PropTypes.number,
+    avatar: PropTypes.shape({
+      url: PropTypes.string,
+    }),
+  }),
+};
+
+MiniDefaultAvatar.defaultProps = {
+  deliveryman: PropTypes.shape({
+    avatar_id: 0,
+  }),
 };
